d7bac0acee946c994563e15ab90eb40a5d535f14
Michael Han <xiangyu.han@ucalgary.ca>
2020-04-02 11:21:44 -0600

    debug

--- injection.c
+++ injection.c
@@ -36,6 +36,9 @@ void send_synack(const int, const char *, const char *, const uint32_t, const ui
     const uint16_t, const uint16_t, uint32_t, uint32_t);
 void send_pshack(const int, const char *, const char *, const uint32_t, const uint32_t, 
     const uint16_t, const uint16_t, uint32_t, uint32_t);
+void print_ip_header(unsigned char* , int);
+void print_tcp_packet(unsigned char* , int);
+void PrintData (unsigned char* , int);
 // void sigint_handler(int);
 void sigchld_handler(int);
 
@@ -47,6 +50,12 @@ struct pshdr {
   uint8_t protocol;
   uint16_t tcp_len;
 };
+
+// Global Vars
+int sock_raw;
+FILE *logfile;
+int i,j;
+struct sockaddr_in source,dest;
 
 int main(int argc, char *argv[]) {
     int attack_sock, sniff_sock, count = 0, num, rv, yes = 1;
@@ -66,6 +75,8 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in sa;
     char *data;
 
+    logfile=fopen("injection.log","w+");
+
     csa.sa_handler = sigchld_handler; // reap all dead processes
 	sigemptyset(&csa.sa_mask);
 	csa.sa_flags = SA_RESTART;
@@ -75,7 +86,7 @@ int main(int argc, char *argv[]) {
 	}
 
     if (argc != 3) {
-	    fprintf(stderr,"usage: reset service_ip service_port\n");
+	    fprintf(stderr,"usage: injection service_ip service_port\n");
 	    exit(1);
 	}
 
@@ -162,10 +173,11 @@ int main(int argc, char *argv[]) {
             if (iph -> protocol != IPPROTO_TCP) goto final; // check if packet is TCP packet
             if (tcph -> syn != 1) goto final; // only care about syn packets
             if ((iph -> daddr != service_addr) && (tcph -> dest != service_port)) goto final; // destination has to be the service
-            // print_tcp_packet(buf, num); // log the packet
+            
+            print_tcp_packet(buf, num); // log the packet
 
-            send_synack(attack_sock, datagram, pseudo_packet, iph->daddr, iph->saddr, tcph->dest, tcph->source, ntohl(tcph->ack_seq) + 1, ntohl(tcph->seq)); // syn ack
-            send_pshack(attack_sock, datagram, pseudo_packet, iph->daddr, iph->saddr, tcph->dest, tcph->source, ntohl(tcph->ack_seq) + 2, ntohl(tcph->seq)); // psh ack
+            // send_synack(attack_sock, datagram, pseudo_packet, iph->daddr, iph->saddr, tcph->dest, tcph->source, ntohl(tcph->ack_seq) + 1, ntohl(tcph->seq)); // syn ack
+            // send_pshack(attack_sock, datagram, pseudo_packet, iph->daddr, iph->saddr, tcph->dest, tcph->source, ntohl(tcph->ack_seq) + 2, ntohl(tcph->seq)); // psh ack
             goto final;
         }
     }
@@ -315,5 +327,119 @@ void send_pshack(const int attack_sock, const char *datagram, const char *pseudo
     if ((num = sendto(attack_sock, datagram, sizeof(struct iphdr) + tcp_len, 0, (struct sockaddr *) &sa, sizeof sa)) < 0)
     {
         perror("fakesync: sendto()\n");
+    }
+}
+
+void print_ip_header(unsigned char* Buffer, int Size)
+{
+    unsigned short iphdrlen;
+         
+    struct iphdr *iph = (struct iphdr *)Buffer;
+    iphdrlen =iph->ihl*4;
+     
+    memset(&source, 0, sizeof(source));
+    source.sin_addr.s_addr = iph->saddr;
+     
+    memset(&dest, 0, sizeof(dest));
+    dest.sin_addr.s_addr = iph->daddr;
+     
+    fprintf(logfile,"\n");
+    fprintf(logfile,"IP Header\n");
+    fprintf(logfile,"   |-IP Version        : %d\n",(unsigned int)iph->version);
+    fprintf(logfile,"   |-IP Header Length  : %d DWORDS or %d Bytes\n",(unsigned int)iph->ihl,((unsigned int)(iph->ihl))*4);
+    fprintf(logfile,"   |-Type Of Service   : %d\n",(unsigned int)iph->tos);
+    fprintf(logfile,"   |-IP Total Length   : %d  Bytes(Size of Packet)\n",ntohs(iph->tot_len));
+    fprintf(logfile,"   |-Identification    : %d\n",ntohs(iph->id));
+    //fprintf(logfile,"   |-Reserved ZERO Field   : %d\n",(unsigned int)iphdr->ip_reserved_zero);
+    //fprintf(logfile,"   |-Dont Fragment Field   : %d\n",(unsigned int)iphdr->ip_dont_fragment);
+    //fprintf(logfile,"   |-More Fragment Field   : %d\n",(unsigned int)iphdr->ip_more_fragment);
+    fprintf(logfile,"   |-TTL      : %d\n",(unsigned int)iph->ttl);
+    fprintf(logfile,"   |-Protocol : %d\n",(unsigned int)iph->protocol);
+    fprintf(logfile,"   |-Checksum : %d\n",ntohs(iph->check));
+    fprintf(logfile,"   |-Source IP        : %s\n",inet_ntoa(source.sin_addr));
+    fprintf(logfile,"   |-Destination IP   : %s\n",inet_ntoa(dest.sin_addr));
+}
+ 
+void print_tcp_packet(unsigned char* Buffer, int Size)
+{
+    unsigned short iphdrlen;
+     
+    struct iphdr *iph = (struct iphdr *)Buffer;
+    iphdrlen = iph->ihl*4;
+     
+    struct tcphdr *tcph=(struct tcphdr*)(Buffer + iphdrlen);
+             
+    fprintf(logfile,"\n\n***********************TCP Packet*************************\n");    
+         
+    print_ip_header(Buffer,Size);
+         
+    fprintf(logfile,"\n");
+    fprintf(logfile,"TCP Header\n");
+    fprintf(logfile,"   |-Source Port      : %u\n",ntohs(tcph->source));
+    fprintf(logfile,"   |-Destination Port : %u\n",ntohs(tcph->dest));
+    fprintf(logfile,"   |-Sequence Number    : %u\n",ntohl(tcph->seq));
+    fprintf(logfile,"   |-Acknowledge Number : %u\n",ntohl(tcph->ack_seq));
+    fprintf(logfile,"   |-Header Length      : %d DWORDS or %d BYTES\n" ,(unsigned int)tcph->doff,(unsigned int)tcph->doff*4);
+    //fprintf(logfile,"   |-CWR Flag : %d\n",(unsigned int)tcph->cwr);
+    //fprintf(logfile,"   |-ECN Flag : %d\n",(unsigned int)tcph->ece);
+    fprintf(logfile,"   |-Urgent Flag          : %d\n",(unsigned int)tcph->urg);
+    fprintf(logfile,"   |-Acknowledgement Flag : %d\n",(unsigned int)tcph->ack);
+    fprintf(logfile,"   |-Push Flag            : %d\n",(unsigned int)tcph->psh);
+    fprintf(logfile,"   |-Reset Flag           : %d\n",(unsigned int)tcph->rst);
+    fprintf(logfile,"   |-Synchronise Flag     : %d\n",(unsigned int)tcph->syn);
+    fprintf(logfile,"   |-Finish Flag          : %d\n",(unsigned int)tcph->fin);
+    fprintf(logfile,"   |-Window         : %d\n",ntohs(tcph->window));
+    fprintf(logfile,"   |-Checksum       : %d\n",ntohs(tcph->check));
+    fprintf(logfile,"   |-Urgent Pointer : %d\n",tcph->urg_ptr);
+    fprintf(logfile,"\n");
+    fprintf(logfile,"                        DATA Dump                         ");
+    fprintf(logfile,"\n");
+         
+    fprintf(logfile,"IP Header\n");
+    PrintData(Buffer,iphdrlen);
+         
+    fprintf(logfile,"TCP Header\n");
+    PrintData(Buffer+iphdrlen,tcph->doff*4);
+         
+    fprintf(logfile,"Data Payload\n");  
+    PrintData(Buffer + iphdrlen + tcph->doff*4 , (Size - tcph->doff*4-iph->ihl*4) );
+                         
+    fprintf(logfile,"\n###########################################################");
+}
+ 
+void PrintData (unsigned char* data , int Size)
+{
+     
+    for(i=0 ; i < Size ; i++)
+    {
+        if( i!=0 && i%16==0)   //if one line of hex printing is complete...
+        {
+            fprintf(logfile,"         ");
+            for(j=i-16 ; j<i ; j++)
+            {
+                if(data[j]>=32 && data[j]<=128)
+                    fprintf(logfile,"%c",(unsigned char)data[j]); //if its a number or alphabet
+                 
+                else fprintf(logfile,"."); //otherwise print a dot
+            }
+            fprintf(logfile,"\n");
+        } 
+         
+        if(i%16==0) fprintf(logfile,"   ");
+            fprintf(logfile," %02X",(unsigned int)data[i]);
+                 
+        if( i==Size-1)  //print the last spaces
+        {
+            for(j=0;j<15-i%16;j++) fprintf(logfile,"   "); //extra spaces
+             
+            fprintf(logfile,"         ");
+             
+            for(j=i-i%16 ; j<=i ; j++)
+            {
+                if(data[j]>=32 && data[j]<=128) fprintf(logfile,"%c",(unsigned char)data[j]);
+                else fprintf(logfile,".");
+            }
+            fprintf(logfile,"\n");
+        }
     }
 }
