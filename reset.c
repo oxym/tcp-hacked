f70690984dcca04fc13adc14809d50a33aee62d6
Michael Han <xiangyu.han@ucalgary.ca>
2020-04-02 01:47:00 -0600

    multiprocess

--- reset.c
+++ reset.c
@@ -118,26 +118,27 @@ int main(int argc, char *argv[]) {
             exit(1);
         }
 
-        unsigned short iphdrlen;
-     
-        iph = (struct iphdr *) buf;
-        tcph= (struct tcphdr*) (buf + iph->ihl * 4);
+        if (!fork()) {
+            iph = (struct iphdr *) buf;
+            tcph= (struct tcphdr*) (buf + iph->ihl * 4);
 
-        if (iph -> protocol != IPPROTO_TCP) continue; // check if packet is TCP packet
-        if (iph -> daddr == service_addr) {
-            if (tcph -> dest != service_port) continue; //check if destination port matches
-        } else if (iph -> saddr == service_addr) {
-            if (tcph -> source != service_port) continue; //check if destination port matches
-        } else {
-            continue;
+            if (iph -> protocol != IPPROTO_TCP) goto final; // check if packet is TCP packet
+            if (iph -> daddr == service_addr) {
+                if (tcph -> dest != service_port) goto final; //check if destination port matches
+            } else if (iph -> saddr == service_addr) {
+                if (tcph -> source != service_port) goto final; //check if destination port matches
+            } else {
+                goto final;
+            }
+            print_tcp_packet(buf, num); // log the packet
+            // reset(iph->saddr, iph->daddr, tcph->source, tcph->dest, tcph->seq, tcph->ack_seq); // reset the server
+            // reset(iph->daddr, iph->saddr, tcph->dest, tcph->source, tcph->ack_seq, tcph->seq); // reset the sender
+            goto final;
         }
-        print_tcp_packet(buf, num); // log the packet  
-
-        // reset(iph->saddr, iph->daddr, tcph->source, tcph->dest, tcph->seq, tcph->ack_seq); // reset the server
-        // reset(iph->daddr, iph->saddr, tcph->dest, tcph->source, tcph->ack_seq, tcph->seq); // reset the sender
-
-        // break; // comment to unleash
     }
+
+final:
+    close(logfile);
     close(sockfd);
     return 0;
 }
